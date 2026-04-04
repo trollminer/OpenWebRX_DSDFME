5a79aea8aac2543e3e3109b0fade41573e471454
trollminer <32915724+trollminer@users.noreply.github.com>
2026-04-04 01:04:55 +0100

    Delete overlay/python/owrx directory

--- overlay/python/owrx/jsons.py
+++
@@ -1,9 +0,0 @@
-from owrx.property import PropertyManager
-import json
-
-
-class Encoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, PropertyManager):
-            return o.__dict__()
-        return super().default(o)
